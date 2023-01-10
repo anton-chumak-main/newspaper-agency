f8a3b014fe0291d497f243819d9d9f275ddb7863
Anton Chumak <Chumak7377@gmail.com>
2023-01-10 15:49:08 +0200

    Create models & migrations

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -100,13 +100,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "agency.Redactor"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
