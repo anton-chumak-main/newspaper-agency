442c798ea85524bbc8442495230fbad4725bbe46
Anton Chumak <Chumak7377@gmail.com>
2023-01-19 17:17:58 +0200

    Update settings

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%*wot5o3pgc_%g0llix*2ssg=o!1oo^33ruz=rh+1^4gza6l5a"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-%*wot5o3pgc_%g0llix*2ssg=o!1oo^33ruz=rh+1^4gza6l5a")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "agency.Redactor"
 
-LOGIN_REDIRECT_URL = "/agency/"
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
